fb7245427457068ec1e02454bd862553297d90b5
YellowFlash1901 <vardaansathe@gmail.com>
2024-12-17 14:58:09 +0000

    improved scaling

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Navbar.css'
 function Navbar() {
   return (
     <nav className="bg-primary shadow-md">
       <div className="container mx-auto flex justify-between items-center p-4">
-      <a href="/" className="text-2xl font-bold text-accent" onClick={(e) => {
-              e.preventDefault(); 
-              window.location.href = '/'; 
-          }}>
-              Vardaan Sathe
-          </a>
+      <a href="/" className="font-bold text-accent resizeable-text" onClick={(e) => {
+            e.preventDefault(); 
+            window.location.href = '/'; 
+        }}>
+            Vardaan Sathe
+        </a>
+
         <ul className="flex space-x-6 text-white">
           <li><Link to="/" className="hover:text-accent">Home</Link></li>
           <li><Link to="/about" className="hover:text-accent">About</Link></li>
