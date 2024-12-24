d3263de68bed5640f4d69e6b281aad3a4fae30d7
YellowFlash1901 <vardaansathe@gmail.com>
2024-12-24 02:14:14 +0000

    bug fixed

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <div className="flex-grow">
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -5,20 +5,33 @@ function Navbar() {
   return (
     <nav className="bg-gray-800 shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center p-4">
-        <a href="/home" className="text-2xl font-bold text-cyan-400 hover:text-cyan-300">
+        <a href="/" className="text-2xl font-bold text-cyan-400 hover:text-cyan-300">
           Vardaan Sathe
         </a>
         <ul className="flex space-x-6">
-          {['Home', 'About', 'Projects', 'Contact'].map((item) => (
+        <li>
+              <Link
+                to={`/`}
+                className="text-gray-300 hover:text-cyan-400 transition-colors duration-300"
+              >
+
+                Home
+              </Link>
+              
+            </li>
+          {[ 'About', 'Projects', 'Contact'].map((item) => (
             <li key={item}>
               <Link
                 to={`/${item.toLowerCase()}`}
                 className="text-gray-300 hover:text-cyan-400 transition-colors duration-300"
               >
+
                 {item}
               </Link>
+              
             </li>
           ))}
+                     
         </ul>
       </div>
     </nav>
